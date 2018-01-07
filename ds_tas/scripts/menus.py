e77f3402cd9b6903b541b91388b518da42f6084c
David Ellis <ducksual@gmail.com>
2018-01-07 15:11:36 +0000

    Fast levelling function cleanup and docstring.

--- ds_tas/scripts/menus.py
+++ ds_tas/scripts/menus.py
@@ -31,11 +31,48 @@ joy = KeySequence([
 ])
 
 
-def level_fast(vit, att, end, stre, dex, res, inte, fth):
+def level_fast(
+        vitality=0,
+        attunement=0,
+        endurance=0,
+        strength=0,
+        dexterity=0,
+        resistance=0,
+        intelligence=0,
+        faith=0
+):
+    """
+    Level up as quickly as possible. Any unused parameters won't be levelled.
+
+    :param vitality:
+    :param attunement:
+    :param endurance:
+    :param strength:
+    :param dexterity:
+    :param resistance:
+    :param intelligence:
+    :param faith:
+    :return: Keysequence for level up
+    """
     # Assume you start in the level up window with vit highlighted
     inc_lvl = right + wait
     seq = KeySequence()
-    for stat in [vit, att, end, stre, dex, res, inte, fth]:
-        seq += inc_lvl * (stat - 1) + right
+    stats = [
+        vitality,
+        attunement,
+        endurance,
+        strength,
+        dexterity,
+        resistance,
+        intelligence,
+        faith,
+    ]
+    for stat in stats:
+        if stat > 0:
+            seq += inc_lvl * (stat - 1) + right
+        else:
+            # if we don't level a stat we need to wait
+            # a frame so the next down press registers.
+            seq += wait
         seq += down
     return seq
